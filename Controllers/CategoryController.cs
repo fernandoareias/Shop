ccbba95f38b0e560b89dfc93c36863093074e470
Fernando <nando.calheirosx@gmail.com>
2021-07-23 13:32:31 -0300

    Scaffold Task and AciontResults

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Models;
+
 
 
 // Endpoint => URL
@@ -8,36 +11,53 @@ using Shop.Models;
 public class CategoryController : ControllerBase
 {
 
+   [HttpGet]
+   [Route("")]
+   public async Task<ActionResult<List<Category>>> Get()
+   {
+      return Ok(new List<Category>());
+   }
+
 
    [HttpGet]
    // Verifica se o valor passado pela URL é inteiro
    [Route("{id:int}")]
-   public string GetById(int id)
+   public async Task<ActionResult<Category>> GetById(int id)
    {
-      return $"GET {id}";
+      return Ok(new Category());
    }
 
 
    [HttpPost]
    [Route("")]
 
-   public Category Post([FromBody] Category model)
+   public async Task<ActionResult<Category>> Post([FromBody] Category model)
    {
-      return model;
+      if (!ModelState.IsValid)
+         return BadRequest(ModelState);
+      return Ok(model);
    }
 
    [HttpPut]
-   [Route("")]
-   public string Put()
+   [Route("{id:int}")]
+   public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
    {
-      return "PUT";
+      // Verificar se o ID informado pela URL é o mesmo da categoria
+      if (id != model.Id)
+         return NotFound(new { message = "Categoria não encontrada" });
+
+      // Verifica se o modelo passado é válido
+      // Caso não retorna a ErrorMessage apropriada 
+      if (!ModelState.IsValid)
+         return BadRequest(ModelState);
+      return Ok(model);
    }
 
    [HttpDelete]
-   [Route("")]
-   public string Delete()
+   [Route("{id:int}")]
+   public async Task<ActionResult<Category>> Delete(int id)
    {
-      return "DELETE";
+      return Ok();
    }
 
 
