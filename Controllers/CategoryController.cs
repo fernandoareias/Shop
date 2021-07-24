228f788275707357937934da553c99e6272c2ff8
Fernando <nando.calheirosx@gmail.com>
2021-07-24 11:24:19 -0300

    CRUD In Memory

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
 using Shop.Models;
 
 
@@ -13,34 +16,55 @@ public class CategoryController : ControllerBase
 
    [HttpGet]
    [Route("")]
-   public async Task<ActionResult<List<Category>>> Get()
+   public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
    {
-      return Ok(new List<Category>());
+      // Realiza apenas uma leitura rapida dos dados no DB in Memory e retorna
+      // ToList é sempre no final, ele realiza as operações no banco
+      var categoryList = await context.Categorys.AsNoTracking().ToListAsync();
+
+      return Ok(categoryList);
+
    }
 
 
    [HttpGet]
    // Verifica se o valor passado pela URL é inteiro
    [Route("{id:int}")]
-   public async Task<ActionResult<Category>> GetById(int id)
+   public async Task<ActionResult<Category>> GetById(int id, [FromServices] DataContext context)
    {
-      return Ok(new Category());
+      // Busca no banco a categoria que possui o ID passado pela URL e retorna 
+      var category = await context.Categorys.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+      return Ok(category);
    }
 
 
    [HttpPost]
    [Route("")]
 
-   public async Task<ActionResult<Category>> Post([FromBody] Category model)
+   public async Task<ActionResult<Category>> Post([FromBody] Category model, [FromServices] DataContext context)
    {
       if (!ModelState.IsValid)
          return BadRequest(ModelState);
-      return Ok(model);
+
+      try
+      {
+         // Add model in DB Memory
+         context.Categorys.Add(model);
+
+         // Persist Category in DB
+         await context.SaveChangesAsync();
+         return Ok(model);
+      }
+      catch
+      {
+
+         return BadRequest(new { message = "Não foi possivel criar a categoria" });
+      }
    }
 
    [HttpPut]
    [Route("{id:int}")]
-   public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
+   public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model, [FromServices] DataContext context)
    {
       // Verificar se o ID informado pela URL é o mesmo da categoria
       if (id != model.Id)
@@ -50,14 +74,48 @@ public class CategoryController : ControllerBase
       // Caso não retorna a ErrorMessage apropriada 
       if (!ModelState.IsValid)
          return BadRequest(ModelState);
-      return Ok(model);
+
+      try
+      {
+         // Verifica automaticamente a modificação de valores e a realiza
+         context.Entry<Category>(model).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+         await context.SaveChangesAsync();
+         return Ok(model);
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+
+         return BadRequest(new { message = "Essa categoria ja foi atualizada" });
+      }
+      catch (Exception)
+      {
+
+         return BadRequest(new { message = "Não foi possivel atualizar a categoria" });
+      }
    }
 
    [HttpDelete]
    [Route("{id:int}")]
-   public async Task<ActionResult<Category>> Delete(int id)
+   public async Task<ActionResult<Category>> Delete(int id, [FromServices] DataContext context)
    {
-      return Ok();
+      // Obtem a categoria que possui o ID passado pela URL
+      var category = await context.Categorys.FirstOrDefaultAsync(x => x.Id == id);
+      if (category == null)
+         return NotFound(new { message = "Categoria não encontrada" });
+
+      try
+      {
+         // Remove a categoria encontrada anteriormente
+         context.Categorys.Remove(category);
+         await context.SaveChangesAsync();
+         return Ok(new { message = "Categoria removida com sucesso " });
+      }
+      catch (Exception)
+      {
+
+         return BadRequest(new { message = "Não foi possivel remover a categoria" });
+      }
+
    }
 
 
