8d759a061c8d85a868d557eeaabe88127beb0afc
Fernando <nando.calheirosx@gmail.com>
2021-07-25 13:57:35 -0300

    Excpetions

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -8,117 +8,138 @@ using Shop.Models;
 
 
 
-// Endpoint => URL
-// https://127.0.01:5001/categories
-[Route("categories")]
-public class CategoryController : ControllerBase
+namespace Shop.Controllers
 {
-
-   [HttpGet]
-   [Route("")]
-   public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+   // Endpoint => URL
+   // https://127.0.01:5001/categories
+   [Route("categories")]
+   public class CategoryController : ControllerBase
    {
-      // Realiza apenas uma leitura rapida dos dados no DB in Memory e retorna
-      // ToList é sempre no final, ele realiza as operações no banco
-      var categoryList = await context.Categorys.AsNoTracking().ToListAsync();
 
-      return Ok(categoryList);
+      [HttpGet]
+      [Route("")]
+      public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
+      {
+         try
+         {
+            // Realiza apenas uma leitura rapida dos dados no DB in Memory e retorna
+            // ToList é sempre no final, ele realiza as operações no banco
+            var categoryList = await context.Categorys.AsNoTracking().ToListAsync();
+
+            return Ok(categoryList);
+         }
+         catch (Exception)
+         {
+
+            return BadRequest(new { message = "Não foi possivel retornar as categorias" });
+         }
+
+      }
+
+
+      [HttpGet]
+      // Verifica se o valor passado pela URL é inteiro
+      [Route("{id:int}")]
+      public async Task<ActionResult<Category>> GetById(int id, [FromServices] DataContext context)
+      {
+         try
+         {
+
+            // Busca no banco a categoria que possui o ID passado pela URL e retorna 
+            var category = await context.Categorys.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+               return BadRequest(new { message = "Não foi possivel encontrar a categoria" });
+            return Ok(category);
+         }
+         catch (Exception)
+         {
+            return BadRequest(new { message = "Não foi possivel encontrar a categoria" });
+
+         }
+      }
+
+
+      [HttpPost]
+      [Route("")]
+
+      public async Task<ActionResult<Category>> Post([FromBody] Category model, [FromServices] DataContext context)
+      {
+         if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+         try
+         {
+            // Add model in DB Memory
+            context.Categorys.Add(model);
+
+            // Persist Category in DB
+            await context.SaveChangesAsync();
+            return Ok(model);
+         }
+         catch
+         {
+
+            return BadRequest(new { message = "Não foi possivel criar a categoria" });
+         }
+      }
+
+      [HttpPut]
+      [Route("{id:int}")]
+      public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model, [FromServices] DataContext context)
+      {
+         // Verificar se o ID informado pela URL é o mesmo da categoria
+         if (id != model.Id)
+            return NotFound(new { message = "Categoria não encontrada" });
+
+         // Verifica se o modelo passado é válido
+         // Caso não retorna a ErrorMessage apropriada 
+         if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+         try
+         {
+            // Verifica automaticamente a modificação de valores e a realiza
+            context.Entry<Category>(model).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(model);
+         }
+         catch (DbUpdateConcurrencyException)
+         {
+
+            return BadRequest(new { message = "Essa categoria ja foi atualizada" });
+         }
+         catch (Exception)
+         {
+
+            return BadRequest(new { message = "Não foi possivel atualizar a categoria" });
+         }
+      }
+
+      [HttpDelete]
+      [Route("{id:int}")]
+      public async Task<ActionResult<Category>> Delete(int id, [FromServices] DataContext context)
+      {
+         // Obtem a categoria que possui o ID passado pela URL
+         var category = await context.Categorys.FirstOrDefaultAsync(x => x.Id == id);
+         if (category == null)
+            return NotFound(new { message = "Categoria não encontrada" });
+
+         try
+         {
+            // Remove a categoria encontrada anteriormente
+            context.Categorys.Remove(category);
+            await context.SaveChangesAsync();
+            return Ok(new { message = "Categoria removida com sucesso " });
+         }
+         catch (Exception)
+         {
+
+            return BadRequest(new { message = "Não foi possivel remover a categoria" });
+         }
+
+      }
+
+
 
    }
-
-
-   [HttpGet]
-   // Verifica se o valor passado pela URL é inteiro
-   [Route("{id:int}")]
-   public async Task<ActionResult<Category>> GetById(int id, [FromServices] DataContext context)
-   {
-      // Busca no banco a categoria que possui o ID passado pela URL e retorna 
-      var category = await context.Categorys.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
-      
-      return Ok(category);
-   }
-
-
-   [HttpPost]
-   [Route("")]
-
-   public async Task<ActionResult<Category>> Post([FromBody] Category model, [FromServices] DataContext context)
-   {
-      if (!ModelState.IsValid)
-         return BadRequest(ModelState);
-
-      try
-      {
-         // Add model in DB Memory
-         context.Categorys.Add(model);
-
-         // Persist Category in DB
-         await context.SaveChangesAsync();
-         return Ok(model);
-      }
-      catch
-      {
-
-         return BadRequest(new { message = "Não foi possivel criar a categoria" });
-      }
-   }
-
-   [HttpPut]
-   [Route("{id:int}")]
-   public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model, [FromServices] DataContext context)
-   {
-      // Verificar se o ID informado pela URL é o mesmo da categoria
-      if (id != model.Id)
-         return NotFound(new { message = "Categoria não encontrada" });
-
-      // Verifica se o modelo passado é válido
-      // Caso não retorna a ErrorMessage apropriada 
-      if (!ModelState.IsValid)
-         return BadRequest(ModelState);
-
-      try
-      {
-         // Verifica automaticamente a modificação de valores e a realiza
-         context.Entry<Category>(model).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-         await context.SaveChangesAsync();
-         return Ok(model);
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-
-         return BadRequest(new { message = "Essa categoria ja foi atualizada" });
-      }
-      catch (Exception)
-      {
-
-         return BadRequest(new { message = "Não foi possivel atualizar a categoria" });
-      }
-   }
-
-   [HttpDelete]
-   [Route("{id:int}")]
-   public async Task<ActionResult<Category>> Delete(int id, [FromServices] DataContext context)
-   {
-      // Obtem a categoria que possui o ID passado pela URL
-      var category = await context.Categorys.FirstOrDefaultAsync(x => x.Id == id);
-      if (category == null)
-         return NotFound(new { message = "Categoria não encontrada" });
-
-      try
-      {
-         // Remove a categoria encontrada anteriormente
-         context.Categorys.Remove(category);
-         await context.SaveChangesAsync();
-         return Ok(new { message = "Categoria removida com sucesso " });
-      }
-      catch (Exception)
-      {
-
-         return BadRequest(new { message = "Não foi possivel remover a categoria" });
-      }
-
-   }
-
-
-
 }
